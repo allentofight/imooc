b8ba77d4e46da745471d3560ef9690405e5b9ab5
xiekun <xiekun605746@163.com>
2014-11-09 23:24:19 +0800

    完成分类后台录入以及分类存储

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,17 +1,25 @@
 var Movie = require('../models/movie')
-
+var Category = require('../models/category')
 // index page
 exports.index = function(req, res) {
     console.log('user in session:')
     console.log(req.session.user)
 
-    Movie.fetch(function(err, movies) {
+    Category.find({}).populate({
+        path: 'movies',
+        options: {
+            limit: 5
+        }
+    }).exec(function(err, categories) {
         if (err) {
             console.log(err)
         }
+
         res.render('index', {
             title: 'imooc 首页',
-            movies: movies
+            categories: categories
         })
+
     })
+
 }
