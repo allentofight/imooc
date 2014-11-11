4626ea874ab955972f44e580a2972bd64ce831c6
xiekun <xiekun605746@163.com>
2014-11-11 22:23:25 +0800

    电影录入增加分类自定义

--- app/controllers/movie.js
+++ app/controllers/movie.js
@@ -77,19 +77,35 @@ exports.save = function(req, res) {
         })
     } else {
         _movie = new Movie(movieObj)
-        var categoryId = _movie.category
+
+        var categoryId = movieObj.category
+        var categoryName = movieObj.categoryName
+
         _movie.save(function(err, movie) {
             if (err) {
                 console.log(err)
             }
+            if (categoryId) {
+                Category.findById(categoryId, function(err, category) {
+                    category.movies.push(movie._id)
 
-            Category.findById(categoryId, function(err, category) {
-                category.movies.push(movie._id)
-                category.save(function(err, category) {
-                    res.redirect('/movie/' + movie.id)
+                    category.save(function(err, category) {
+                        res.redirect('/movie/' + movie._id)
+                    })
                 })
-            })
+            } else if (categoryName) {
+                var category = new Category({
+                    name: categoryName,
+                    movies: [movie._id]
+                })
 
+                category.save(function(err, category) {
+                    movie.category = category._id
+                    movie.save(function(err, movie) {
+                        res.redirect('/movie/' + movie._id)
+                    })
+                })
+            }
         })
     }
 }
