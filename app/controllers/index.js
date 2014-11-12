bd60433e5d70bb25ae918aa43dab5d3315359eab
xiekun <xiekun605746@163.com>
2014-11-12 08:26:40 +0800

    增加分类列表及分页

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -23,3 +23,42 @@ exports.index = function(req, res) {
     })
 
 }
+
+// search page
+exports.search = function(req, res) {
+    var catId = req.query.cat
+    var page = parseInt(req.query.p)
+    var count = 2
+    var index = page * 2
+
+
+    Category.find({
+        _id: catId
+    }).populate({
+        path: 'movies',
+        select: 'title poster',
+        //分页方法１ 可行!
+        // options: {
+        //     limit: 2,
+        //     skip: index
+        // }
+    }).exec(function(err, categories) {
+        if (err) {
+            console.log(err)
+        }
+        var category = categories[0] || {}
+        var movies = category.movies || []
+        var results = movies.slice(index, index + count)
+
+        res.render('results', {
+            title: 'imooc 结果列表',
+            keyword: category.name,
+            currentPage: page + 1,
+            query: 'cat=' + catId,
+            totalPage: Math.ceil(movies.length / count),
+            movies: results
+        })
+
+    })
+
+}
